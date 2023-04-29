5e7f5bcdf47887d5bf9514fb323ed4bed9218d05
WhiteThunder <whetethunger@gmail.com>
2023-04-29 12:56:01 -0700

    Initial commit

---
+++ LimitTrainLength.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oxide.Plugins
+{
+    [Info("Limit Train Length", "WhiteThunder", "1.0.0")]
+    [Description("Limits the max length of trains.")]
+    internal class LimitTrainLength : CovalencePlugin
+    {
+        #region Fields
+
+        private Configuration _config;
+        private readonly object False = false;
+
+        #endregion
+
+        #region Hooks
+
+        private void Init()
+        {
+            if (_config.TrainLengthLimit < 0)
+            {
+                Unsubscribe(nameof(CanTrainCarCouple));
+            }
+        }
+
+        private object CanTrainCarCouple(TrainCar trainCar, TrainCar otherTrainCar)
+        {
+            if (trainCar.completeTrain.NumTrainCars + otherTrainCar.completeTrain.NumTrainCars <= _config.TrainLengthLimit)
+                return null;
+
+            return False;
+        }
+
+        #endregion
+
+        #region Configuration
+
+        [JsonObject(MemberSerialization.OptIn)]
+        private class Configuration : BaseConfiguration
+        {
+            [JsonProperty("Train length limit")]
+            public int TrainLengthLimit = -1;
+        }
+
+        private Configuration GetDefaultConfig() => new Configuration();
+
+        #region Configuration Helpers
+
+        [JsonObject(MemberSerialization.OptIn)]
+        private class BaseConfiguration
+        {
+            private string ToJson() => JsonConvert.SerializeObject(this);
+
+            public Dictionary<string, object> ToDictionary() => JsonHelper.Deserialize(ToJson()) as Dictionary<string, object>;
+        }
+
+        private static class JsonHelper
+        {
+            public static object Deserialize(string json) => ToObject(JToken.Parse(json));
+
+            private static object ToObject(JToken token)
+            {
+                switch (token.Type)
+                {
+                    case JTokenType.Object:
+                        return token.Children<JProperty>()
+                                    .ToDictionary(prop => prop.Name,
+                                                  prop => ToObject(prop.Value));
+
+                    case JTokenType.Array:
+                        return token.Select(ToObject).ToList();
+
+                    default:
+                        return ((JValue)token).Value;
+                }
+            }
+        }
+
+        private bool MaybeUpdateConfig(BaseConfiguration config)
+        {
+            var currentWithDefaults = config.ToDictionary();
+            var currentRaw = Config.ToDictionary(x => x.Key, x => x.Value);
+            return MaybeUpdateConfigSection(currentWithDefaults, currentRaw);
+        }
+
+        private bool MaybeUpdateConfigSection(Dictionary<string, object> currentWithDefaults, Dictionary<string, object> currentRaw)
+        {
+            var changed = false;
+
+            foreach (var key in currentWithDefaults.Keys)
+            {
+                object currentRawValue;
+                if (currentRaw.TryGetValue(key, out currentRawValue))
+                {
+                    var defaultDictValue = currentWithDefaults[key] as Dictionary<string, object>;
+                    var currentDictValue = currentRawValue as Dictionary<string, object>;
+
+                    if (defaultDictValue != null)
+                    {
+                        if (currentDictValue == null)
+                        {
+                            currentRaw[key] = currentWithDefaults[key];
+                            changed = true;
+                        }
+                        else if (MaybeUpdateConfigSection(defaultDictValue, currentDictValue))
+                            changed = true;
+                    }
+                }
+                else
+                {
+                    currentRaw[key] = currentWithDefaults[key];
+                    changed = true;
+                }
+            }
+
+            return changed;
+        }
+
+        protected override void LoadDefaultConfig() => _config = GetDefaultConfig();
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                _config = Config.ReadObject<Configuration>();
+                if (_config == null)
+                {
+                    throw new JsonException();
+                }
+
+                if (MaybeUpdateConfig(_config))
+                {
+                    PrintWarning("Configuration appears to be outdated; updating and saving");
+                    SaveConfig();
+                }
+            }
+            catch (Exception e)
+            {
+                PrintError(e.Message);
+                PrintWarning($"Configuration file {Name}.json is invalid; using defaults");
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void SaveConfig()
+        {
+            Puts($"Configuration changes saved to {Name}.json");
+            Config.WriteObject(_config, true);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Features
+
+Allows limiting the max length of trains.
+
+## Configuration
+
+Default configuration:
+
+```json
+{
+  "Train length limit": -1
+}
+```
+
+- `Train length limit` -- Determines the max length of trains. Set to `-1` to have no limit.
